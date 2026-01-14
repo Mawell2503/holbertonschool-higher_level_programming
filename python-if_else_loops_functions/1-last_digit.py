32ad6a16d7c9e804a5e2831d9e99b75386f78877
Your Name <you@example.com>
2026-01-14 14:26:43 +0400

    m

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,11 +11,11 @@ if last_digit < 6 and last_digit != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
         number, last_digit
         ))  # one way of cutting a line is just to \n  where needed
-            # string is different, place a \ before \n
+        
 if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(
         number, last_digit
-        ))  
+        ))
 
 if last_digit == 0:
     print("Last digit of {:d} is {:d} and is 0".format(
