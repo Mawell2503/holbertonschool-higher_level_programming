a14f74fae947a121d0e0c8c6e21d8cb66b0403f9
Your Name <you@example.com>
2026-01-17 16:49:02 +0400

    m

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num != 99:
-        print("{:02d}, ".format(num), end="")  #{:02d} = prints integer with a 0 pad
+        print("{:02d}, ".format(num), end="")  #{:02d} = prinnt integer with a 0 pad
     else:
         print("{:02d}".format(n))
