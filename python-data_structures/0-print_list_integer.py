87fe5a8e28069da74de26967b8b123d4261e4a9e
Your Name <you@example.com>
2026-01-20 12:53:25 +0400

    m

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3 
-def print_list_integer(matrix=[[]]):
-    for row in matrix:
+def print_list_integer(my_list=[[]]):
+    for row in my_list:
         for i, num in enumerate(row):
             if i != len(row) -1:
                 print("{:d}".format(num), end=" ")
