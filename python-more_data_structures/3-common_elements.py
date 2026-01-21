0978fdf53687471e9bcf4cfda8e1aa1566c14935
Your Name <you@example.com>
2026-01-21 17:13:42 +0400

    m

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    #  create set
     common = set()
+    #  loop through set_1
     for element in set_1:
+        #  while looping through set_1 loop through set_2 and check for element
         if element in set_2:
+            #  add element to set
             common.add(element)
+            #  return the set
             return common
