79dd757ba1599218dda79f8ba2fd399592c9f650
Your Name <you@example.com>
2026-01-22 12:41:49 +0400

    m

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    #  variable for the count of keys
     count = 0
+    #  loop through dictionary
     for key in a_dictionary:
+        #  count adding up
         count = count + 1
     return count
--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    #  check and sort a_dictionary then place it in sorted_keys
     sorted_keys = sorted(a_dictionary.keys())
+    #  loop through sorted_keys
     for key in sorted_keys:
+        #  print the sorted items
         print(f"{key}: {a_dictionary[key]}")
