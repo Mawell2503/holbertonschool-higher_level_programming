0db031dacb3477dc9f942e14bfb6b4177b5ad113
Your Name <you@example.com>
2026-01-22 12:23:25 +0400

    m

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -8,10 +8,10 @@ def only_diff_elements(set_1, set_2):
         if element not in set_2:
             #  add element in set
             diff.add(element)
-        #  loop through set_2
-        for element in set_2:
-            # if element is not in set_1
-            if element not in set_1:
-                #  add element in set
-                diff.add(element)
+    #  loop through set_2
+    for element in set_2:
+        # if element is not in set_1
+        if element not in set_1:
+            #  add element in set
+            diff.add(element)
     return diff
