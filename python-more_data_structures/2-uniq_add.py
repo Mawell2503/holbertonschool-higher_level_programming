a768f5056acadaec0b81a2241ae5205ac41de7b6
Your Name <you@example.com>
2026-01-22 12:13:40 +0400

    m

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,13 +4,13 @@ def uniq_add(my_list=[]):
     unique_values = set()
     #  loop through the list
     for value in my_list:
-        #  add the values to the set(.add() only add an item once) 
+        #  add the values to the set(.add() only add an item once)
         unique_values.add(value)
         #  create variable for the addition
     total = 0
     # loop through the new set
     for value in unique_values:
-        #add every value 
+        #  add every value
         total = total + value
     #  return the addition
     return total
