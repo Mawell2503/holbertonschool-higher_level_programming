1716db8b833f5718210baf1bd4d7096f4f50e832
Your Name <you@example.com>
2026-01-23 13:06:59 +0400

    m

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x = 0):
+def safe_print_list(my_list=[], x=0):
     #  initializing the number of elements that needs to be returned
     count = 0
     #  the equivalent in C is for (int i = 0; i < x; i++)
