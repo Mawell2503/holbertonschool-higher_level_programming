5385e0231f5c5fceeb5e5fc35bd8f2d07447b32f
Your Name <you@example.com>
2026-01-28 11:31:50 +0400

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""This file defines a Square class."""
 #  create class
 class Square:
-    """This file defines a class Square."""
+    """This class does nothing """
     #  "do nothing for now"
     pass
