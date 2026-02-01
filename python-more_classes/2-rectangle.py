45c2641ba7d0ba5883a6a369d0893e9dd9763648
Your Name <you@example.com>
2026-02-01 21:33:30 +0400

    m

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,1 +1,46 @@
 #!/usr/bin/python3
+"""This module defines a rectangle"""
+
+
+class Rectangle:
+    """This class defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """This method creates a private instance attribute width """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """This method retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This methods works in pair with property as its value setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """This method retrieves the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This methods works in pair with property as its value setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 *(self.__height + self.__width)
--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""This module defines a Rectangle"""
+
+
+class Rectangle:
+    """This class is a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """This module creates a private instance attribute"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """This module is to retrieve width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """This module is to set the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if width > 0:
+            raise ValueError("width must be >= 0")
+        
+    @property
+    def height(self):
+        """This module is to retrieve height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """This module is to set the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height musr be ann integer")
+        if height < 0:
+            raise (ValueError(" heigh must be >= 0"))
