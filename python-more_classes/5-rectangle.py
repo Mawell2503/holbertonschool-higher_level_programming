f4ddf537233282db2ff4e17640ee293aefc00808
Your Name <you@example.com>
2026-02-01 22:49:33 +0400

    m

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle"""
+
+
+class Rectangle:
+    """This class is a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """This module creates a private instance attribute"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """This module is to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This module is to set the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """This module is to retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This module is to set the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise (ValueError("height must be >= 0"))
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        line = []
+        for i in range(self.__height):
+            line.append("#" * self.__width)
+        return "\n" .join(line)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
