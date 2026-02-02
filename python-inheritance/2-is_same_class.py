1b8ea3e3481ef1bb480a14d69198ebf0f9da3f4a
Your Name <you@example.com>
2026-02-02 15:09:24 +0400

    m

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""wallah"""
+"""this module is wallah"""
 
 
 def is_same_class(obj, a_class):
-    """astafallah"""
+    """this function is astafallah"""
     if type(obj) == a_class:
         return True
     return False
