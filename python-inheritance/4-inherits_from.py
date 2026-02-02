e3251ea402503f688d13ce666b6c8717465d735c
Your Name <you@example.com>
2026-02-02 19:26:27 +0400

    m

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """This function is sbhtsnrdbr"""
+    """Return True if obj is an instance of a subclass of a_class"""
     #  checks if object is subclass OF class a_class or is an instance
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
