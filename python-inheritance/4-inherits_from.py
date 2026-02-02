4bfef977f237298787e99495d90fd79537660452
Your Name <you@example.com>
2026-02-02 16:25:44 +0400

    m

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+##!/usr?bin/python3
+def is_kind_of_class(obj, a_class):
+    """This function is sbhtsnrdbr"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
