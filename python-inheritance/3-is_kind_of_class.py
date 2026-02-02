aab9635e24a80d36771401a790d850696c9ca108
Your Name <you@example.com>
2026-02-02 16:17:20 +0400

    m

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@
 
 def is_kind_of_class(obj, a_class):
     """This function is sbhtsnrdbr"""
-    if isinstance (obj, a_class):
-        return True 
+    if isinstance(obj, a_class):
+        return True
     return False
