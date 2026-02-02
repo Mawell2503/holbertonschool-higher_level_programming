0585445c0c322c18c51017b3c5a0381ad090d91b
Your Name <you@example.com>
2026-02-02 20:01:24 +0400

    m

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """This module is something ig"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """Return True if obj is an instance of a subclass of a_class"""
     #  checks if object is subclass OF class a_class or is an instance
     if isinstance(obj, a_class) and type(obj) is not a_class:
