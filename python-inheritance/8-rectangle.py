882bd9a9644a0c03feb3d28e2e46961fb93fd8f2
Your Name <you@example.com>
2026-02-02 23:40:13 +0400

    documentation is shit

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -15,6 +15,7 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """This class is a child"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
