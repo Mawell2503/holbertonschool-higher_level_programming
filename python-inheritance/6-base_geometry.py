270aafeb9a9a2dd4ef5299ca887cef91fbbe43be
Your Name <you@example.com>
2026-02-02 20:23:32 +0400

    FIRST TRY BABYYYYYYYYY

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module is the follow up"""
+
+
+class BaseGeometry:
+    """This method raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
