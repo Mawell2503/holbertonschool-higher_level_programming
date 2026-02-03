2fb513844166e9f8b7d31498f238c1fd88aa998b
Your Name <you@example.com>
2026-02-03 12:15:40 +0400

    m

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,8 +2,16 @@
 """This module is the follow up"""
 
 
-#  Bring in the BaseGeometry class from its fie
-from base_geometry import BaseGeometry
+class BaseGeometry:
+    """This method raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"<name> must be an integer")
+        if value <= 0:
+            raise ValueError(f"<name> must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """This class is a child"""
