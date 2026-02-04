3610af2cee790a1551a579d7abc15b1476ad2fed
Your Name <you@example.com>
2026-02-04 17:32:23 +0400

    m

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,6 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """This class is a child"""
     def __init__(self, width, height):
@@ -26,4 +27,3 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
