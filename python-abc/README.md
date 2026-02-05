71b2e0f48493161329723a3035104ff852325024
Your Name <you@example.com>
2026-02-05 22:36:29 +0400

    m

--- python-abc/README.md
+++ python-abc/README.md
@@ -1,5 +1,11 @@
-Twelfth Projects/ Abstract Classes and Interfaces
+# Twelfth Projects/ Abstract Classes and Interfaces
 *ABC > Abstract Bases Classes
 
 - an abstract class is a class that cannot be instantiated(;to create an object from a class[obj = ClassName()]) directly
 
+## TASK EXPLANATION
+task 0:
+    ~creating and abstract class animal.
+    import module: from module_name import item_name
+
+task 1:
--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""Importing a abstract class"""
+from abc import ABC, abstractmethod
 
+
+class Animal(ABC):
+    """Abstract base class for animals"""
+
+    @abstractmethod
+    def sound(self):
+        """Return animal sound"""
+        pass
+
+class Dog(Animal):
+    """Dog subclass"""
+
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """Cat subclass"""
+
+    def sound(self):
+        return "Meow"
