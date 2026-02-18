30eb0ea1c22a0094867179953c7e60c8364c5203
Your Name <you@example.com>
2026-02-18 15:00:44 +0400

    avoy non bad zot p dir

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print ("")
+
+        else:
+            for i in range(self.__size):
+                print("#" * self.size)
