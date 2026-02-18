fbdfe6d8ad8d375c97a873864925993374ca2a99
Your Name <you@example.com>
2026-02-18 14:27:51 +0400

    m

--- python-abc/main_01_duck_typing.py
+++ python-abc/main_01_duck_typing.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
-from task_01_duck_typing import Circle, Rectangle, shape_info
+from abc import ABC, abstractmethod
+from math import pi
 
-circle = Circle(radius=5)
-rectangle = Rectangle(width=4, height=7)
+class Shape(ABC):
+    def area(self):
+        pass
 
-shape_info(circle)
-shape_info(rectangle)
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width * self.height)
---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from abc import ABC
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+
+class Rectangle(Shape):
+    
