7934eb1508150825ba41e9db22212083706260ed
Your Name <you@example.com>
2026-02-20 12:19:45 +0400

    m

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -14,7 +14,15 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
+        self.check_radius(radius)
         self.radius = radius
+
+    def check_radius(self, value):
+        self.radius = value
+        if not isinstance(value, int):
+            raise TypeError("radius must be an integer")
+        if value < 0:
+            raise ValueError("radius is negative")
         
     def area(self):
         return pi * self.radius ** 2
@@ -24,9 +32,25 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
+        self.check_width(width)
+        self.check_height(height)
         self.width = width
         self.height = height
 
+    def check_width(self, value):
+        self.width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width is negative")
+        
+    def check_height(self, value):
+        self.height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height is negative")
+        
     def area(self):
         return self.height * self.width
     
