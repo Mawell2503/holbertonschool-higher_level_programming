ca5f7c87570f5d9a778d17d771a0fd31bd4569f1
Your Name <you@example.com>
2026-02-20 12:38:03 +0400

    m

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -28,13 +28,13 @@ class Circle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        if not isinstance(value, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        if not isinstance(value, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.width = width
         self.height = height
