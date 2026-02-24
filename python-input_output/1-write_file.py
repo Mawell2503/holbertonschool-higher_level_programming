f8fcc572ca1f035a6fd8ad502ed5733c7f14420b
Your Name <you@example.com>
2026-02-24 23:15:59 +0400

    m

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns number of characters written"""
+    """Returns number of characters written"""
     #  open "filename" on overide mode and convert characters > bytes
     #  + link this to my_f
     with open(filename, "w", encoding="utf-8") as my_f:
