286bd45a55c25b2e4c8769282ea25936722c6282
Your Name <you@example.com>
2026-02-24 13:44:45 +0400

    m

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+#  filename="" is basically a variable for the file u will input
+
 def read_file(filename=""):
+    #  open is a built in function that open a file
+    #  encoding="utf-8" is basically what the function must translate the content of the file too
+    #  utf-8 is a character encoding system
+    #  as f is basically n likimama handle
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
---
+++ python-input_output/README.md
@@ -0,0 +1,7 @@
+~13th Project/ Input and Output~
+
+What is utf-8?
+- Its a character encoding system.
+- Its a unicode transformation format > Unicode is a universal standard that assigns every character in every language.
+
+~Task Explanation~
