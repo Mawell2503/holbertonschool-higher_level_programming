f3fc67fca05d448e8b1953cdc7b99ca1f06bdd41
Your Name <you@example.com>
2026-02-24 13:49:02 +0400

    m

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 #  filename="" is basically a variable for the file u will input
 
 def read_file(filename=""):
+    """Reads a utf-8 text fie and prnts it"""
     #  open is a built in function that open a file
     #  encoding="utf-8" is basically what the function must translate the content of the file too
     #  utf-8 is a character encoding system
