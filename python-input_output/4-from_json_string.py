d1cf246e00f93ad8a012df16b1e3438ae92ec84d
Your Name <you@example.com>
2026-02-25 21:55:32 +0400

    m

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Module that provides a function to convert a JSON string to a python object"""
+"""Module returns an object"""
 
 import json
 
 
 def from_json_string(my_str):
     """Returns the Python object represented by a JSON string."""
-    #  json.loads is used to read JSON data from a file and convert it into a Python object
+    #  json.loads: read JSON data and convert it into a Python object
     #  JSON string > Python
     return json.loads(my_str)
