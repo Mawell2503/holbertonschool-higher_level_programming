29b015b0396bb4a0224fc15d90e663ede550d0b7
Your Name <you@example.com>
2026-03-03 13:54:44 +0400

    m

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-class CounterIterator:
+class CountedIterator:
     def __init__(self, iterobj):
         #  iter() is a python built-in funct. that turns iterable into iterator
+        #  It stores the original iterator
         self.iterobj = iter(iterobj)
         self.counter =  0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            self.counter += 1
+            #  normally next() will automatically raise stopiterations
+            return next(self.iterobj)
+        except StopIteration:
+            raise StopIteration
         
     def get_count(self):
-        return self.counter
+        return self.counter 
+       
