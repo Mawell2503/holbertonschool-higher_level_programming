d2e3a9bf56ec28eb5a75c21aa11626dff528684d
Your Name <you@example.com>
2026-03-06 10:53:44 +0400

    m

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from 5_save_to_json_file import save_to_json_file
+from 6_load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+items = load_from_json_file(filename)
