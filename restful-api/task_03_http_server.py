8ca89833872e5fe0bb007f4cfa1949e450f90568
Your Name <you@example.com>
2026-04-03 15:08:36 +0400

    m

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,28 +16,51 @@ class APIHandler(BaseHTTPRequestHandler):
         #  > you call this before sending the body  
         #  self.wfile is a writeable stream used to send data 
         #  self.wfile.write sends the response body
+        #  200 is the response code when the server succesfully received request
+        #  404 is when it failed
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("Hello this is your API!!".encode())
+            self.wfile.write(b"Hello this is your API!!")
 
         elif self.path == "/data":
-            data ={
+            data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
-                }
-            self.send_response()
-            self.send_header()
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write()
+            self.wfile.write(json.dumps(data).encode())
 
         elif self.path == "/status":
-            self.send_response()
-            self.send_header()
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write()
+            self.wfile.write("OK".encode())
 
         elif self.path == "/info":
-            
+            info = {
+                "version": "1.0",
+                "description": "A simple APPI built with http.server"
+            }
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Endpoint not found".encode())
+
+port=8000
+
+if __name__ == "__main__":
+    server_address =("0.0.0.0", port)
+    httpd = HTTPServer(server_address, APIHandler)
+    print(f"Server running on http://{server_address[0]}:{port}")
+    httpd.serve_forever()
